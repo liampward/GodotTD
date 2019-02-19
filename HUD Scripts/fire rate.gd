0fedf64f25e17a8400b3f254a5febad1b860525d
kelby8 <kschick94@outlook.com>
2019-02-19 13:02:21 -0500

    Add files via upload

---
+++ HUD Scripts/fire rate.gd
@@ -0,0 +1,19 @@
+extends TextEdit
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	pass
+
+func _process(delta):
+	# Called every frame. Delta is time since last frame.
+	# Update game logic here.
+	if main.selection != null:
+		self.text = main.selection.fire_rate
+	else:
+		self.text = ""
+	pass
