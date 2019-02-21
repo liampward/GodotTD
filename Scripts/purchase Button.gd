0175d66ac941ab6145115ed85ad80309deb5333f
Liam P. Ward <lpward97@yahoo.com>
2019-02-21 13:17:26 -0500

    new root
    New root?

--- Scripts/purchase Button.gd
+++ Scripts/purchase Button.gd
@@ -1,9 +1,15 @@
 extends Button
 
 var root_node
+var board_node
 
 func _ready():
 	root_node = get_tree().get_root().get_node("Root")
+	board_node = root_node.get_child("Board")
 
 func _pressed():
-	root_node.mode = "purchase"
+	var tile = board_node.selected_tile
+	
+	if(tile.tower == null):
+		var new_tower = preload("res://Scenes/baseTower.tscn");
+		tile.add_child(new_tower);
