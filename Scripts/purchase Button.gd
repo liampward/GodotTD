2b98518b24c0ce1e82152715386c5453c34eec31
Liam P. Ward <lpward97@yahoo.com>
2019-02-21 13:24:00 -0500

    We're Buying Towers
    Towers are being purchased.

--- Scripts/purchase Button.gd
+++ Scripts/purchase Button.gd
@@ -2,14 +2,17 @@ extends Button
 
 var root_node
 var board_node
+var new_tower_resource = preload("res://Scenes/baseTower.tscn");
 
 func _ready():
 	root_node = get_tree().get_root().get_node("Root")
-	board_node = root_node.get_child("Board")
+	board_node = root_node.get_node("Board")
+	new_tower_resource = preload("res://Scenes/baseTower.tscn");
 
 func _pressed():
 	var tile = board_node.selected_tile
 	
 	if(tile.tower == null):
-		var new_tower = preload("res://Scenes/baseTower.tscn");
+		var new_tower = new_tower_resource.instance();
+		new_tower.set_translation(Vector3(0, 10, 0))
 		tile.add_child(new_tower);
