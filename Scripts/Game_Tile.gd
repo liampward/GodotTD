358fd5cb9c12783bcfd30e7e030a22c4f87475b3
yimmit303 <timsteiner303@gmail.com>
2019-02-21 12:21:01 -0500

    Changed Board behavoir
    Added Selection Indication, added tower variable in the tile class

---
+++ Scripts/Game_Tile.gd
@@ -0,0 +1,23 @@
+extends MeshInstance
+
+# class member variables go here, for example:
+# var a = 2
+var tower = null
+var selected = false
+var moved = false
+
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	pass
+	
+func _process(delta):
+	if selected == true and moved == false:
+		self.translation.y += 0.3
+		self
+		moved = true
+	if selected == false and moved == true:
+		self.translation.y -= 0.3
+		moved = false
+
