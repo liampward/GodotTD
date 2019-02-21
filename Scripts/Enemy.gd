0ff524b6df4ee2b91a172fc910ed48ea742d3a65
m-gaston <35315240+m-gaston@users.noreply.github.com>
2019-02-21 14:10:55 -0500

    Enemy's color now changes based on health

--- Scripts/Enemy.gd
+++ Scripts/Enemy.gd
@@ -1,10 +1,23 @@
 extends MeshInstance
 
-export onready var hp = 10
-export onready var speed = 5
+const MOVE_SPEED = 5
+
+var hp_max = 100.0
+var hp_cur = hp_max
+
+var matl = SpatialMaterial.new()
 
 func _ready():
-	pass
+	self.set_material_override(matl)
+	matl.albedo_color = Color(1, 0, 0)
 
 func _process(delta):
-	translation += Vector3(1, 0, 0) * speed * delta;
+	#translation += Vector3(1, 0, 0) * MOVE_SPEED * delta
+
+	if hp_cur <= 0:
+		queue_free()
+		# TODO add money to player's balance
+
+func hurt(dmg):
+	hp_cur -= dmg
+	matl.albedo_color = Color(hp_cur / hp_max, 0, 0)
