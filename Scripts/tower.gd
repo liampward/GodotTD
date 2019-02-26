7a5f0539d55f8dd411f9087317842f0563ce7930
Fr0gKn1ght <47430696+Fr0gKn1ght@users.noreply.github.com>
2019-02-26 12:46:16 -0500

    Modified Towers
    Additional functionality to towers.
    
    Only attacks enemies
    
    Basic bullet fire

--- Scripts/tower.gd
+++ Scripts/tower.gd
@@ -1,32 +1,48 @@
 extends Spatial
+signal intruder
 
-class Tower:
-	var aggroDist
-	var attackType
-	var cost
-	var effect
-	var aoe
-	#var upgradeTime
-	#var paths
-	
-	func _ready():
-		return
-	
-	func check():
-		#If enemy is in range
-		#Then call attack.
-		return
-	
-	func attack():
-		#Generate attack directed at closest detected enemy
-		#Should there be some sort of attack class/enum?
-		return
-	
-	#Maybe for a Tower/GameObject manager?
-	func upgrade():
-		#Create an instance of a tower from one of the upgrad paths in place of this instance
-		#Delete this instance
-		return
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+var BULLET = preload("res://Scenes/Bullet.tscn")
 
-	func _process():
-		return
+export onready var price = 5
+
+var interval = 5
+var canFire = true
+
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	preload("res://Scripts/Bullet.gd")
+	print("Turret Exists")
+	pass
+	
+func attack():
+	if canFire:
+		var bullet = BULLET.instance()
+		bullet.set_name("myBullet")
+		bullet.set_transform(get_node("SpawnLoc").global_transform)
+		get_parent().add_child(bullet)
+		canFire = false
+		
+	
+
+func _process(delta):
+	if !canFire:
+		interval -= delta
+		if interval <= 0:
+			interval = 5
+			canFire = true
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
+
+
+func _on_Area_area_entered(badGuy):
+	if badGuy.get_parent().is_in_group("ENEMY"):
+		attack();
+	#emit_signal("intruder")
+	#attack();
+	pass # replace with function body
