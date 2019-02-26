3f90945203aa47e61906c9cdd00de75bdbd78a98
yimmit303 <timsteiner303@gmail.com>
2019-02-26 13:00:06 -0500

    Added greater HUD functionality

--- Scripts/range.gd
+++ Scripts/range.gd
@@ -1,13 +1,18 @@
 extends TextEdit
 
 var root_node
+var board_node
 
 func _ready():
-	root_node = get_tree().get_root().get_node("Root");
+	root_node = get_tree().get_root().get_node("Root")
+	board_node = root_node.get_node("Board")
 
 func _process(delta):
-	if root_node.selected != null:
-		self.text = root_node.selected.Range
+	if board_node.selected_tile != null:
+		if board_node.selected_tile.tower != null:
+			self.text = "100"
+		else:
+			self.text = ""
 	else:
 		self.text = ""
 	pass
