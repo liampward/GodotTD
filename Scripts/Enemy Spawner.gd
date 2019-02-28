8277feb990d55216d1e1c42eca3f12c20aa1f02b
EmmOriold <EmmOriold@Gmail.com>
2019-02-28 13:49:13 -0500

    fixed duplicate nodes
    broke spawning but that's being worked on

--- Scripts/Enemy Spawner.gd
+++ Scripts/Enemy Spawner.gd
@@ -7,29 +7,28 @@ var Enemy_Timer
 var Enemy
 
 func _ready():
-	# Called when the node is added to the scene for the first time.
-	# Initialization here
-	Root_Node = get_tree().get_root().get_node("Root")
-	Board_Node = Root_Node.get_node("Board")
-	Enemy_Timer = Root_Node.get_node("EnemyTimer")
-	Enemy = load("res://Scenes/Enemy.tscn")
+    # Called when the node is added to the scene for the first time.
+    # Initialization here
+    Root_Node = get_tree().get_root().get_node("Root")
+    Board_Node = Root_Node.get_node("Board")
+    Enemy = load("res://Scenes/Enemy.tscn")
 
 func _process(delta):
-	# Called every frame. Delta is time since last frame.
-	# Update game logic here.
-	pass
+    # Called every frame. Delta is time since last frame.
+    # Update game logic here.
+    pass
 
 func Spawn():
-	var Clone = Enemy.instance()
-	Root_Node.add_child(Clone)
-	var size = Board_Node.board_size
-	Clone.set_translation(Vector3(0, 0, 0))
-	Board_Node.ignore_list.append(Clone.get_node("Area"))
+    var Clone = Enemy.instance()
+    Root_Node.add_child(Clone)
+    var size = Board_Node.board_size
+    Clone.set_translation(Vector3(0, 0, 0))
+    Board_Node.ignore_list.append(Clone.get_node("Area"))
 
 func SpawnWave(T):
-	for i in range(0,T):
-		Spawn()
-		yield(get_tree().create_timer(1.0), "timeout")
+    for i in range(0,T):
+        Spawn()
+        yield(get_tree().create_timer(1.0), "timeout")
 
 func _on_EnemyTimer_timeout():
-	self.SpawnWave(3)
+    self.SpawnWave(3)
