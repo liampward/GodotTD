0137f759ee18af2a32ae345ec583c62fad75a2ef
yimmit303 <timsteiner303@gmail.com>
2019-03-04 23:29:40 -0500

    Fixed memory leak

--- Scripts/Enemy.gd
+++ Scripts/Enemy.gd
@@ -9,9 +9,13 @@ var matl = SpatialMaterial.new()
 
 var DEATH = preload("res://Scenes/EnemyDeathSound.tscn")
 var particle_scene
+var board_node
+var root_node
 
 func _ready():
 	particle_scene = preload("res://Scenes/SmokeParticle.tscn")
+	root_node = get_tree().get_root().get_node("Root")
+	board_node = root_node.get_node("Board")
 	self.set_material_override(matl)
 	matl.albedo_color = Color(1, 0, 0)
 
@@ -21,6 +25,10 @@ func _process(delta):
 	if hp_cur <= 0:
 		var deathSound = DEATH.instance()
 		self.get_parent().add_child(deathSound)
+		
+		#MAKE SURE THIS LINE IS CALLED IF THE ENEMY EVER DIES!!!!
+		board_node.ignore_list.remove(board_node.ignore_list.find($Area))
+		
 		queue_free()
 		var particle_spawner = particle_scene.instance()
 		get_parent().add_child(particle_spawner)
@@ -35,6 +43,7 @@ func hurt(dmg):
 
 func _on_Area_area_entered(area):
 	if area.get_parent().is_in_group("BULLET"):
+		board_node.ignore_list.remove(board_node.ignore_list.find(area))
 		area.get_parent().queue_free()
 		hurt(area.get_parent().damage)
 
