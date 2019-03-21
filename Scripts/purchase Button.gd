896f5353b6ef8dcf464308c2220f915e1d180d5c
EmmOriold <EmmOriold@Gmail.com>
2019-03-21 13:05:52 -0400

    Upgrade Buttons now Purchase
    Can buy any tower to start with, moved functionality of purchase button to upgrade buttons.

--- Scripts/purchase Button.gd
+++ Scripts/purchase Button.gd
@@ -2,23 +2,14 @@ extends Button
 
 var root_node
 var board_node
-var new_tower_resource
+var NEUTRAL
+var PHYSICAL
+var MAGIC
 
 func _ready():
 	root_node = get_tree().get_root().get_node("Root")
 	board_node = root_node.get_node("Board")
-	new_tower_resource = preload("res://Scenes/baseTower.tscn");
-
-func _pressed():
-	if(board_node.selected_tile != null):
-		var tile = board_node.selected_tile
-		var tile_scale = tile.get_scale();
-		
-		if(tile.tower == null && root_node.money >= 10):
-			var new_tower = new_tower_resource.instance();
-			new_tower.set_translation(Vector3(0, 3, 0))
-			new_tower.set_scale(Vector3(1, 1 / tile_scale.y, 1))
-			tile.add_child(new_tower)
-			tile.tower = new_tower
-			board_node.ignore_list.append(new_tower.get_node("Area"))
-			root_node.money -= 10
+	NEUTRAL = preload("res://Scenes/baseTower.tscn")
+	PHYSICAL = preload("res://Scenes/physTower.tscn")
+	MAGIC = preload("res://Scenes/magicTower.tscn")
+	
