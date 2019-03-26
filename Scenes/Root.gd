e520f7d17d9ccaa40980f7b87d591de420d33443
yimmit303 <timsteiner303@gmail.com>
2019-03-26 13:52:04 -0400

    Removed Unused code

--- Scenes/Root.gd
+++ Scenes/Root.gd
@@ -1,21 +1,15 @@
 extends Node
 
-enum Mode{ NONE, SELL, BUY }
-
-var money;
-var mode;
-var selected;
-var health;
+var money
+var health
 
 func _ready():
 	money = 100;
 	health = 10;
-	mode = Mode.NONE;
-	selected = null;
-
 
 	#OS.set_window_fullscreen(true)
 
 func _process(delta):
 	if(Input.is_key_pressed(KEY_ESCAPE)):
 		get_tree().quit()
+
