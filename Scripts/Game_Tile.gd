0cde088f5bc110a8e9fdf3ea981058555646f1ca
yimmit303 <timsteiner303@gmail.com>
2019-03-26 13:52:47 -0400

    Made the tile different on selection

--- Scripts/Game_Tile.gd
+++ Scripts/Game_Tile.gd
@@ -2,21 +2,26 @@ extends MeshInstance
 
 # class member variables go here, for example:
 # var a = 2
+var matl = SpatialMaterial.new()
+var old_matl
 var tower = null
 var selected = false
 var moved = false
 
 
 func _ready():
-	# Called when the node is added to the scene for the first time.
-	# Initialization here
-	pass
+	old_matl = self.get_surface_material(0)
+	#matl.albedo_color = Color(0.4, 0.7, 0.4)
+	matl.emission_enabled = true
+	matl.emission = Color(0.13, 0.8, 0.13)
+	matl.emission_energy = 1
 	
 func _process(delta):
 	if selected == true and moved == false:
 		self.translation.y += 0.3
-		self
+		self.set_surface_material(0, matl)
 		moved = true
 	if selected == false and moved == true:
 		self.translation.y -= 0.3
+		self.set_surface_material(0, old_matl)
 		moved = false
