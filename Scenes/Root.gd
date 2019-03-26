110f469563857b86ac36b0b1247518a4f8d031b1
yimmit303 <timsteiner303@gmail.com>
2019-03-26 15:24:33 -0400

    Added wave count

--- Scenes/Root.gd
+++ Scenes/Root.gd
@@ -2,14 +2,25 @@ extends Node
 
 var money
 var health
+var wave_num
+var board_node
+var script
 
 func _ready():
-	money = 100;
-	health = 10;
+	money = 100
+	health = 10
+	wave_num = 1
+	
+#	board_node = self.get_node("Board")
+#	var tower_script = load("res://Scripts/TowerPurchase.gd")
+#	script = tower_script.new()
 
-	#OS.set_window_fullscreen(true)
 
 func _process(delta):
 	if(Input.is_key_pressed(KEY_ESCAPE)):
 		get_tree().quit()
-
+#	if(Input.is_key_pressed(KEY_1)):
+#		if board_node.selected_tile.tower != null:
+#			script.upgrade(1)
+#		if board_node.selected_tile.tower == null:
+#			script.purchase("NEUTRAL")
