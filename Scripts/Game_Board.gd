958f9df135af74fd06b28ad06ebcf0efbc426ab6
m-gaston <35315240+m-gaston@users.noreply.github.com>
2019-03-26 09:19:31 -0400

    Added AStarNode and cleaned up Game_Board.gd

--- Scripts/Game_Board.gd
+++ Scripts/Game_Board.gd
@@ -1,8 +1,5 @@
 extends Spatial
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
 var board_size = 10
 var RAY_LENGTH = 100
 var ignore_list = []
@@ -40,25 +37,6 @@ func get_object_under_mouse():
 	var space_state = camera.get_world().direct_space_state
 	var selection = space_state.intersect_ray(ray_from, ray_to, ignore_list)
 	return selection
-
-func a_star(start, goal):
-	var checked_nodes = []
-	
-	var discovered_nodes = []
-	
-	var came_from = {}
-	
-	for i in range(0,board_size):
-		for j in range(0,board_size):
-			came_from[[i,j]] = 1000000
-			
-	came_from[[0,0]]  = 0
-	
-
-func pathfind():
-	pass
-
-
 
 func _on_GameArea_mouse_entered():
 	on_menu = false
