60a48cc3750f50b27be364e6ae48fba7b412ed19
yimmit303 <timsteiner303@gmail.com>
2019-03-27 18:08:41 -0400

    More optimization work

--- Scripts/tower.gd
+++ Scripts/tower.gd
@@ -18,13 +18,17 @@ var interval = fireRate
 var canFire = true
 var upgradeLevel = 0;
 var Stack = []
+var targs = []
 
 func _ready():
 	preload("res://Scripts/Bullet.gd")
 	BULLET = preload("res://Scenes/Bullet.tscn")
-	MAGIC = preload("res://Scenes/magicTower.tscn")	
+	MAGIC = preload("res://Scenes/magicTower.tscn")
 	PHYSICAL = preload("res://Scenes/physTower.tscn")
 	NEUTRAL = preload("res://Scenes/baseTower.tscn")
+	$Area.connect("area_entered", self, "_on_Area_area_entered")
+	$Area.connect("area_exited", self, "_on_Area_area_exited")
+	
 func attack(enemy):
 	if canFire:
 		self.get_node("AudioStreamPlayer").play()
@@ -49,35 +53,29 @@ func _process(delta):
 		if interval <= 0:
 			interval = fireRate
 			canFire = true
-	var targs = self.get_node("Area").get_overlapping_areas()
 	for i in range(len(targs)):
 		if targs[i].get_parent().is_in_group("ENEMY"):
 			attack(targs[i].get_parent())
 			break
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-#	pass
-
 
 func _on_Area_area_entered(badGuy):
 	if badGuy.get_parent().is_in_group("ENEMY"):
-		attack(badGuy.get_parent());
-	#emit_signal("intruder")
-	#attack();
-	pass # replace with function body
+		targs.append(badGuy)
+		
+func _on_Area_area_exited(badGuy):
+	if badGuy.get_parent().is_in_group("ENEMY"):
+		targs.remove(targs.find(badGuy))
 	
 func upgrade(num):
 	price += 10
 	if upgradeLevel < 2:
 		var tower
-		
 		if num == 1:
 			#Neutral Tower
 			tower = NEUTRAL.instance()
 		if num == 2:
 			#Neutral Tower
 			tower = PHYSICAL.instance()
-
 		if num == 3:
 			#Magic Tower
 			tower = MAGIC.instance()
