cb335f423ec4987460df6dd9b3bb35507073f197
yimmit303 <timsteiner303@gmail.com>
2019-04-04 17:05:56 -0400

    More ghost work

--- Scripts/GhostContainer.gd
+++ Scripts/GhostContainer.gd
@@ -1,13 +1,21 @@
 extends Spatial
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
+var root_node
+var board_node
+var towers = []
 
 func _ready():
-	# Called when the node is added to the scene for the first time.
-	# Initialization here
-	pass
+	root_node = get_tree().get_root().get_node("Root")
+	board_node = root_node.get_node("Board")
+	towers.append($GhostNeut)
+	towers.append($GhostPhys)
+	towers.append($GhostMag)
 
-func move_tower():
-	pass
+func move_tower(type):
+	if(board_node.selected_tile != null):
+		if(board_node.selected_tile.tower == null):
+			towers[type].set_translation(board_node.selected_tile.get_translation())
+			towers[type].visible = true
+		else:
+			pass # on 
+		
