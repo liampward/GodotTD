b916641e578599343f9681b3a2611ec8687e9a22
yimmit303 <timsteiner303@gmail.com>
2019-04-11 22:11:22 -0400

    Options Menu Added

--- Scripts/Sell Button.gd
+++ Scripts/Sell Button.gd
@@ -8,9 +8,9 @@ func _ready():
 	board_node = root_node.get_node("Board")
 
 func _pressed():
-	var tile = board_node.selected_tile;
 
-	if(tile.tower != null):
+	if(board_node.selected_tile != null and board_node.selected_tile.tower != null):
+		var tile = board_node.selected_tile;
 		root_node.money += (tile.tower.price - (tile.tower.price / 3));
 		tile.tower.queue_free();
 		tile.tower = null;
