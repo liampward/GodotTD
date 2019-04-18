2672c4eac72a70a959156c02cb3cf50ec9f7815c
Liam P. Ward <lpward97@yahoo.com>
2019-04-18 13:34:19 -0400

    Sell button conflicts fixed

--- Scripts/Sell Button.gd
+++ Scripts/Sell Button.gd
@@ -9,12 +9,11 @@ func _ready():
 
 func _pressed():
 
-<<<<<<< HEAD
 	if(tile.tower != null):
 		board_node.as.add_point(
-					tile.astar_node_id, 
+					tile.astar_node_id,
 		            tile.astar_node_pos)
-		
+
 		var c = board_node.as.get_point_position(tile.astar_node_id)
 		for x in [-1, 0, 1]:
 			for z in [-1, 0, 1]:
@@ -25,11 +24,7 @@ func _pressed():
 					var id2 = board_node.points[board_node.vec3_to_string(c + offset)]
 					if not board_node.as.are_points_connected(tile.astar_node_id, id2):
 						board_node.as.connect_points(tile.astar_node_id, id2, true)
-		
-=======
-	if(board_node.selected_tile != null and board_node.selected_tile.tower != null):
-		var tile = board_node.selected_tile;
->>>>>>> ce74f67b1bf26047bb60b0ad7e4b514921ac440a
+
 		root_node.money += (tile.tower.price - (tile.tower.price / 3));
 		tile.tower.queue_free();
 		tile.tower = null;
