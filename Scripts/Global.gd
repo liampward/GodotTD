ce74f67b1bf26047bb60b0ad7e4b514921ac440a
EmmOriold <EmmOriold@Gmail.com>
2019-04-18 12:58:21 -0400

    GUI Overhaul
    the GUI is Professional
    
    reorganized and added a toggle for number or icon stat display

--- Scripts/Global.gd
+++ Scripts/Global.gd
@@ -3,7 +3,7 @@ extends Node
 var Wave_Count
 var volume
 var num_stats
-
+var numericStats = false
 func _ready():
 	# Called when the node is added to the scene for the first time.
 	# Initialization here
--- Scripts/StatContainer.gd
+++ Scripts/StatContainer.gd
@@ -3,13 +3,20 @@ extends Control
 var root_node
 var board_node
 
-
 func _ready():
 	root_node = get_tree().get_root().get_node("Root")
 	board_node = root_node.get_node("Board")
-	
+
 
 func _process(delta):
+	if Global.numericStats == false:
+		$IconStats.visible = true
+		$NumberStats.visible = false
+	else:
+		$IconStats.visible = false
+		$NumberStats.visible = true
+	
+	
 	if board_node.selected_tile != null and board_node.selected_tile.tower != null:
 		var T = board_node.selected_tile.tower
 		var damCap = max(1, min(5, T.damage / 5.6))		
@@ -17,38 +24,40 @@ func _process(delta):
 		var rateCap = round(max(1, min(5, (1 / T.fireRate) * 2.45)))
 		
 		var i = 0
-		for d in $DamageGrid.get_children():
+		for d in $IconStats/DamageGrid.get_children():
 			if i < damCap:
 				d.get_child(0).play("on")
 			else:
 				d.get_child(0).play("default")
 			i += 1
 		i = 0
-		for d in $RangeGrid.get_children():
+		for d in $IconStats/RangeGrid.get_children():
 			if i < rangCap:
 				d.get_child(0).play("on")
 			else:
 				d.get_child(0).play("default")
 			i += 1
 		i = 0
-		for d in $FireRateGrid.get_children():
+		for d in $IconStats/FireRateGrid.get_children():
 			if i < rateCap:
 				d.get_child(0).play("on")
 			else:
 				d.get_child(0).play("default")
 			i += 1
 		$Name.text = T.stackName
+		$NumberStats/Range.text = str(T.fireRange)
+		$NumberStats/FireRate.text = str(T.fireRate)
+		$NumberStats/Damage.text = str(T.damage)
 
-
-#		$Range.text = str(board_node.selected_tile.tower.fireRange)
-#		$FireRate.text = str(board_node.selected_tile.tower.fireRate)
-#		$Damage.text = str(board_node.selected_tile.tower.damage)
-#		$Name.text = board_node.selected_tile.tower.stackName
 	else:
-		for d in $DamageGrid.get_children():
+		for d in $IconStats/DamageGrid.get_children():
 			d.get_child(0).play("default")		
-		for d in $RangeGrid.get_children():
+		for d in $IconStats/RangeGrid.get_children():
 			d.get_child(0).play("default")
-		for d in $FireRateGrid.get_children():
+		for d in $IconStats/FireRateGrid.get_children():
 			d.get_child(0).play("default")
 		$Name.text = ""
+		$Name.text = ""
+		$NumberStats/Range.text = ""
+		$NumberStats/FireRate.text = ""
+		$NumberStats/Damage.text = ""
