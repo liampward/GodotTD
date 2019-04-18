488a8abd54f15113943f00f43f4350bf6a05ad23
Fr0gKn1ght <47430696+Fr0gKn1ght@users.noreply.github.com>
2019-04-18 13:55:38 -0400

    Particles!
    Particle effect for bullets

--- Scripts/Bullet.gd
+++ Scripts/Bullet.gd
@@ -14,6 +14,7 @@ func move(delta):
 	if targ.get_ref():
 		dir = targ.get_ref().get_global_transform().origin - self.get_global_transform().origin
 		dir = dir.normalized()
+		self.get_node("MeshInstance/Particles").look_at(targ.get_ref().get_global_transform().origin,Vector3(0,1,0))
 	translation += (dir * speed * delta)
 	lifetime -= delta
 	if (lifetime <= 0):
@@ -21,6 +22,7 @@ func move(delta):
 func _ready():
 	root_node = get_tree().get_root().get_node("Root")
 	board_node = root_node.get_node("Board")
+
 
 func _process(delta):
 	move(delta)
