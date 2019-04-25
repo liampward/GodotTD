235acc5d30ab0a293931090cd49f73d124a06fc4
Liam P. Ward <lpward97@yahoo.com>
2019-04-25 12:42:14 -0400

    Hold on
    Don't worry

--- Scripts/Sell Button.gd
+++ Scripts/Sell Button.gd
@@ -8,25 +8,19 @@ func _ready():
 	board_node = root_node.get_node("Board")
 
 func _pressed():
-<<<<<<< HEAD
 	var tile = board_node.selected_tile
-	
+
 	if(tile != null and tile.tower != null):
 		root_node.money += (tile.tower.price - (tile.tower.price / 3));
 		tile.tower.queue_free();
 		tile.tower = null;
-		
-=======
-	var tile = board_node.selected_tile;
 
-	if(tile.tower != null):
->>>>>>> 1ff0a9ebb94e2583bbf1f39d877a40aec1decf09
 		board_node.as.add_point(
 					tile.astar_node_id,
 		            board_node.gm.map_to_world(tile.astar_node_pos.x,
 												tile.astar_node_pos.y,
 												tile.astar_node_pos.z))
-		
+
 		var c = tile.astar_node_pos
 		for x in [-1, 0, 1]:
 			for z in [-1, 0, 1]:
@@ -41,4 +35,3 @@ func _pressed():
 		if(tile.astar_node_id == 97):
 			board_node.as.connect_points(tile.astar_node_id, 1, true)
 		get_tree().call_group("ENEMY", "move_to", Vector3(33, 0, 10))
-		
