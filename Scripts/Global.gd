9d8a62c68b9e8d6b333dc8a25cca16ab07b3346b
yimmit303 <timsteiner303@gmail.com>
2019-04-25 12:38:14 -0400

    Volume and Option menu fix

--- Scripts/Global.gd
+++ Scripts/Global.gd
@@ -2,6 +2,7 @@ extends Node
 
 var Wave_Count
 var volume = 0
+var music_volume = 0
 var numericStats = false
 func _ready():
 	# Called when the node is added to the scene for the first time.
--- Scripts/Pause_Menu.gd
+++ Scripts/Pause_Menu.gd
@@ -3,5 +3,6 @@ extends ColorRect
 func _process(delta):
 	if(Input.is_action_just_pressed("PAUSE")):
 		self.visible = !self.visible
+		$OptionsMenu.visible = false
 		get_tree().paused = !get_tree().paused
-
+		
